eeb19d800db711d71562500b7f6a24ae143ceeee
ossilv01 <ossilv01@ucm.es>
2025-03-10 17:15:36 +0100

    Reapply "Merge branch 'main' into ÓSCAR"
    This reverts commit b79c5f1fce768bb71e624d938a56a00f5a51f434.

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -6,6 +6,7 @@
 //---------------------------------------------------------
 
 using System.Globalization;
+using System.Runtime.CompilerServices;
 using System.Runtime.Serialization.Formatters;
 using Unity.Collections;
 using UnityEngine;
@@ -44,21 +45,27 @@ public class PlayerMovement : MonoBehaviour
     // primera palabra en minúsculas y el resto con la 
     // primera letra en mayúsculas)
     // Ejemplo: _maxHealthPoints
-    [SerializeField] private float horizontal;
-    [SerializeField] private float vertical;
+    
+    
     private Rigidbody2D rb;
-    private bool isGrounded;
-    private GameObject child;
-    private Collider2D childCollider;
+    private GameObject _child;
+    private Collider2D jumpCollider;
+    private Collider2D upLeftCollider;
+    private Collider2D upRightCollider;
+    private Collider2D upCenterCollider;
+
     PlatformMovement platform;
-    [SerializeField]
-    private float jumpTimeCounter;
-    [SerializeField]
-    private float jumpTime;
-    [SerializeField]
+
+
+    private bool isGrounded;
     private bool isJumping;
+    private bool justJumped = false;
+
+    private float jumpTimeCounter;
+    private float jumpTime;
+    
     Vector2 moveInput;
- 
+    
     #endregion
 
     // ---- MÉTODOS DE MONOBEHAVIOUR ----
@@ -75,8 +82,23 @@ public class PlayerMovement : MonoBehaviour
     void Start()
     {
         rb = gameObject.GetComponent<Rigidbody2D>();
-        child = transform.GetChild(0).gameObject; // Obtiene el primer hijo directamente
-        childCollider = child.GetComponent<Collider2D>(); // Obtiene su Collider2D
+
+        #region//Coge referencias a los colliders correspondientes a los hijos
+
+        _child = transform.GetChild(0).gameObject; // Obtiene el primer hijo directamente
+        jumpCollider = _child.GetComponent<Collider2D>(); // Obtiene su Collider2D
+
+        _child = transform.GetChild(1).gameObject; // Obtiene el segundo hijo directamente
+        upLeftCollider = _child.GetComponent<Collider2D>(); // Obtiene su Collider2D
+
+        _child = transform.GetChild(2).gameObject;
+        upRightCollider = _child.GetComponent<Collider2D>();
+
+        _child = transform.GetChild(3).gameObject;
+        upCenterCollider = _child.GetComponent<Collider2D>();
+        #endregion
+
+        justJumped = false;
     }
 
     /// <summary>
@@ -84,69 +106,104 @@ public class PlayerMovement : MonoBehaviour
     /// </summary>
     void Update()
     {
-        if (childCollider == null) return;
+        if (jumpCollider == null) return;
         if (InputManager.Instance == null) return;
 
         //Obtener el vector de movimiento desde el InputManager.
         moveInput = InputManager.Instance.MovementVector;
-        //moveInput.x != 0 ? moveInput.x * speed : rb.velocity.x --> si se está pulsando input se mueve, si no, arrastra la velocidad
-        // rb.velocity = new Vector2(moveInput.x * speed, rb.velocity.y);
-        // rb.velocity = new Vector2(moveInput.x != 0 ? moveInput.x * speed : rb.velocity.x * deacceleration, rb.velocity.y);
-        
 
         //Voltear el sprite
         if (moveInput.x != 0)
         {
             spriteRenderer.flipX = moveInput.x < 0;
         }
-        //Detecta si el hijo está colisionando con algo
-        Collider2D[] hitColliders = Physics2D.OverlapBoxAll(childCollider.bounds.center, childCollider.bounds.size , 0);
 
-        isGrounded = false;
-        platform = null;
-        foreach (var hitCollider in hitColliders)
+
+        //debug:
+        if (isGrounded)
         {
-            if (hitCollider.gameObject != gameObject && ((1 << hitCollider.gameObject.layer) & ground) != 0)
-            {
-                isGrounded = true;
-
-                platform = hitCollider.gameObject.GetComponent<PlatformMovement>();
-                
-            }
+            spriteRenderer.color = Color.white;
+        }
+        else
+        {
+            spriteRenderer.color = Color.yellow;
         }
 
-        
-        
-        #endregion
-
-        //SALTO
-        if (isGrounded && InputManager.Instance.JumpWasPressedThisFrame() && !isJumping)
+        //Detección inputs salto
+        if (isGrounded && InputManager.Instance.JumpWasPressedThisFrame())
         {
-            Debug.Log("Jump Aptempt"+ rb.velocity.y);
-            isJumping = true;
-            //rb.velocity += new Vector2(rb.velocity.x, 0); 
-            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y+ jumpForceInitial);
-            rb.transform.position = new Vector3(transform.position.x,transform.position.y+rb.velocity.y*Time.fixedDeltaTime,transform.position.z);
-            Debug.Log("J: " + rb.velocity.y);
+            justJumped = true;
 
-            jumpTimeCounter = 0;
         }
-        if (InputManager.Instance.JumpWasReleasedThisFrame()&&isJumping)
+        else if (InputManager.Instance.JumpWasReleasedThisFrame() && isJumping)
         {
             isJumping = false;
         }
 
-        
+
 
 
     }
     void FixedUpdate()
     {
-        #region seteo y cambio de velocidad.x, velocidad.y a las correspondientes de base(no contando salto).
-        if (platform == null)
+
+        CollisionDetection();
+
+        SetInitialVelocity();
+
+        JumpCalculations();
+        
+    }
+    #endregion
+
+    // ---- MÉTODOS PÚBLICOS ----
+    #region Métodos públicos
+    // Documentar cada método que aparece aquí con ///<summary>
+    // El convenio de nombres de Unity recomienda que estos métodos
+    // se nombren en formato PascalCase (palabras con primera letra)
+    // mayúscula, incluida la primera letra)
+    // Ejemplo: GetPlayerController
+
+
+
+    #endregion
+
+    // ---- MÉTODOS PRIVADOS ----
+    #region Métodos Privados
+
+    /// <summary>
+    /// se encarga de detectar si el jugador ha colisionado con una plataforma y con el suelo para futuras comprobaciones.
+    /// </summary>
+    private void CollisionDetection()
+    {
+        
+        isGrounded = false;
+        platform = null;
+
+        //Detecta si el jumpCollider está colisionando con algo
+        Collider2D[] hitColliders = Physics2D.OverlapBoxAll(jumpCollider.bounds.center, jumpCollider.bounds.size, 0);
+        foreach (var hitCollider in hitColliders)
+        {
+            if (hitCollider.gameObject != gameObject && ((1 << hitCollider.gameObject.layer) & ground) != 0) //por qué se usa & y no &&?
+            {
+                isGrounded = true;
+                if (hitCollider.gameObject.GetComponent<PlatformMovement>() != null && hitCollider.isTrigger)
+                {
+                    platform = hitCollider.gameObject.GetComponent<PlatformMovement>();
+                }
+            }
+        }
+    }
+
+    /// <summary>
+    /// Setea la velocidad inicial a la correspondiente, dependiendo del input del jugador, de la velocidad previa, y de si está en una plataforma o no.
+    /// </summary>
+    private void SetInitialVelocity()
+    {
+        if(platform == null)
         {
             // si la velocidad mia es mayor a la máxima y no estoy pulsando la tecla de la dir contraria o el 0:
-            if (rb.velocity.x > (moveInput.x * speed) && (rb.velocity.x * moveInput.x) > 0)
+            if ((moveInput.x < 0 && rb.velocity.x < moveInput.x * speed) || (moveInput.x > 0 && rb.velocity.x > moveInput.x * speed))
             {
                 //va al fixedUpdate porque si va a más fps, se resta la deceleración más veces/segundo
                 rb.velocity = new Vector2(rb.velocity.x - deceleration, rb.velocity.y);
@@ -157,11 +214,76 @@ public class PlayerMovement : MonoBehaviour
             }
 
         }
-        else if (platform.getVel() != null && !float.IsNaN(platform.getVel().x) && !float.IsNaN(platform.getVel().y))
+        else if (platform != null)
         {
-            rb.velocity = new Vector2(moveInput.x * speed + platform.getVel().x, platform.getVel().y);
+            rb.velocity = new Vector2(moveInput.x * speed + platform.getVel().x, platform.getVel().y); //el problema estaba aquí. (solucionado haciendo collider más pequeño)
+            //el problema (audio)
+
+        }
+    }
+
+    private void EdgeAlignment()
+    {
+        bool Left = false;
+        bool Right = false;
+        bool Center = false;
+
+        #region Detecta tres colliders de edge alignment y si hay colisión, Lo devuelve en Left, Right y Center
+        Collider2D[] hitColliders = Physics2D.OverlapBoxAll(upLeftCollider.bounds.center, upLeftCollider.bounds.size, 0);
+        foreach (var hitCollider in hitColliders)
+        {
+            if (hitCollider.gameObject != gameObject && ((1 << hitCollider.gameObject.layer) & ground) != 0) //por qué se usa & y no &&?
+            {
+                Left = true;
+            }
+        }
+
+         hitColliders = Physics2D.OverlapBoxAll(upRightCollider.bounds.center, upRightCollider.bounds.size, 0);
+        foreach (var hitCollider in hitColliders)
+        {
+            if (hitCollider.gameObject != gameObject && ((1 << hitCollider.gameObject.layer) & ground) != 0) //por qué se usa & y no &&?
+            {
+                Right = true;
+            }
+        }
+
+        hitColliders = Physics2D.OverlapBoxAll(upCenterCollider.bounds.center, upCenterCollider.bounds.size, 0);
+        foreach (var hitCollider in hitColliders)
+        {
+            if (hitCollider.gameObject != gameObject && ((1 << hitCollider.gameObject.layer) & ground) != 0) //por qué se usa & y no &&?
+            {
+                Center = true;
+            }
         }
         #endregion
+
+        if (Left && !(Right || Center))
+        {
+            //estoy pensando en usar un raycast desde aquí para saber la distancia al borde y así colocar al jugador justo en el borde.
+        }
+        else if (Right && !(Left || Center))
+        {
+
+        }
+    }
+
+    /// <summary>
+    /// Añade las velocidades necesarias para que el jugador salte.
+    /// </summary>
+    private void JumpCalculations()
+    {
+        //salto(inicial)
+        //JustJumped sirve para que se ejecute el impulso de salto inicial una sola vez.
+        if (justJumped)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y + jumpForceInitial);
+            isJumping = true;
+            jumpTimeCounter = 0;
+            justJumped = false;
+
+        }
+
+        //salto(continuación)
         if (isJumping && jumpTimeCounter < jumpTime)
         {
             rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y + jumpForce);
@@ -171,37 +293,8 @@ public class PlayerMovement : MonoBehaviour
         {
             isJumping = false;
         }
+
     }
-    
-
-    // ---- MÉTODOS PÚBLICOS ----
-    #region Métodos públicos
-    // Documentar cada método que aparece aquí con ///<summary>
-    // El convenio de nombres de Unity recomienda que estos métodos
-    // se nombren en formato PascalCase (palabras con primera letra)
-    // mayúscula, incluida la primera letra)
-    // Ejemplo: GetPlayerController
-
-    
-
-    #endregion
-
-    // ---- MÉTODOS PRIVADOS ----
-    #region Métodos Privados
-
-    // Documentar cada método que aparece aquí
-    // El convenio de nombres de Unity recomienda que estos métodos
-    // se nombren en formato PascalCase (palabras con primera letra
-    // mayúscula, incluida la primera letra)
-    //como hipoteticamente podría hacer que el jugador pueda volver a saltar del suelo con un collider como hijo
-    // public Vector 2 MovementVector {get; private set;)
-    /*
-     private void OnMove(InputAction.CallbackContext context)
-    {
-        MovementVector = context.ReadValue<Vector2>();
-    }
-    */
-
 
     #endregion
 
